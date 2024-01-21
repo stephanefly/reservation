a71942c0d2b468d75eefd279203f500825635a2b
FAURE Stephane (SAFRAN AIRCRAFT ENGINES) <stephane.faure@safrangroup.com>
2024-01-21 23:44:07 +0100

    formulaire

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-
-from django import forms
-from .models import Devis
-
-class DevisForm(forms.ModelForm):
-    class Meta:
-        model = Devis
-        fields = ['date', 'adresse', 'code_postal', 'ville']
--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-class Devis(models.Model):
-    date = models.DateField()
-    adresse = models.CharField(max_length=255)
-    code_postal = models.CharField(max_length=10)
-    ville = models.CharField(max_length=100)
-
-class Client(models.Model):
+class Event(models.Model):
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
+    date_evenement = models.DateField()
+    adresse_evenement = models.CharField(max_length=100)
+    ville_evenement = models.CharField(max_length=100, null=True)
+    code_postal_evenement = models.IntegerField(default=75000,
+        validators=[
+            MaxValueValidator(99999),  # Limite maximale à 999999
+            MinValueValidator(9999),       # Limite minimale à 0 (si nécessaire)
+        ])
+    numero_telephone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
