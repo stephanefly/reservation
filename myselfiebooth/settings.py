70b3f8e8adf74c365db8fc3777c168e21147a234
FAURE Stephane (SAFRAN AIRCRAFT ENGINES) <stephane.faure@safrangroup.com>
2024-01-22 20:52:05 +0100

    bdd

--- myselfiebooth/settings.py
+++ myselfiebooth/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'myselfiebooth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservation',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Ou l'adresse IP de votre serveur MySQL
+        'PORT': '3306',        # Le port par défaut de MySQL
     }
 }
 
