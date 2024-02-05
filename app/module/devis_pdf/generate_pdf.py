dfd8a5bc5639f1ae444f12d4fddf05529fe5f7d7
FAURE Stephane (SAFRAN AIRCRAFT ENGINES) <stephane.faure@safrangroup.com>
2024-02-05 22:04:19 +0100

    form confirmation

--- app/module/devis_pdf/generate_pdf.py
+++ app/module/devis_pdf/generate_pdf.py
@@ -18,27 +18,33 @@ def generate_devis_pdf():
     pdf.setFont("Times-Bold", 22)
     pdf.drawString(50, height - 125, "DEVIS n°...")
     pdf.setFont("Helvetica", 12)
+    pdf.setFillColor(colors.darkslategrey)
     pdf.drawString(50, height - 145, "Etabli le XX/XX/XX")
 
     pdf.setFont("Helvetica-Bold", 14)
+    pdf.setFillColor(colors.black)
     pdf.drawString(50, height - 190, "MySelfieBooth")
     pdf.setFont("Helvetica", 12)
+    pdf.setFillColor(colors.darkslategrey)
     pdf.drawString(50, height - 205, "0699733998")
     pdf.drawString(50, height - 220, "contact@myselfiebooth-paris.fr")
     pdf.drawString(50, height - 235, "www.myselfiebooth-paris.fr")
 
     pdf.setFont("Helvetica-Bold", 14)
+    pdf.setFillColor(colors.black)
     pdf.drawString(330, height - 190, "Client Nom")
     pdf.setFont("Helvetica", 12)
+    pdf.setFillColor(colors.darkslategrey)
     pdf.drawString(330, height - 205, "0699733998")
     pdf.drawString(330, height - 220, "contact@myselfiebooth-paris.fr")
 
     pdf.setFont("Helvetica", 11)
+    pdf.setFillColor(colors.black)
     pdf.drawString(50, height - 280, "Location de PHOTOBOOTH - MIROIRBOOTH - 360BOOTH")
 
     # Ajouter un tableau pour les articles
     data = [['Description', 'Prix unitaire', 'QTÉ', 'Réduction', 'TOTAL'],
-            ['Photobooth Tirage Illimtés 5h', '450€', '1', "",'450€'],
+            ['Photobooth Tirage Illimtés 5h', '450€', '1', "50€",'450€'],
             ['Livraison - Installation \n 18/03/2024 \n 77000 Mareuil-les-Meaux ', '0€', '1', "",'0€'],
             ['Personnalisation', '0€', '1', "",'0€'],
             ['Galerie Web', '0€', '1',"", '0€'],
@@ -47,7 +53,7 @@ def generate_devis_pdf():
     table = Table(data, colWidths=[210, 80, 70, 80, 110])
     table_style = [
         ('BACKGROUND', (0, 0), (-1, 0), colors.black),
-        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
         ('FONTNAME', (0, 0), (-1, 0), 'Helvetica'),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 10),
@@ -74,20 +80,25 @@ def generate_devis_pdf():
 
     # Ajouter les totaux et les conditions
     pdf.setFont("Helvetica-Bold", 12)
-    pdf.drawString(400, height - 520, "TOTAL :")
-    pdf.drawString(500, height - 520, "$0.00")
+    pdf.setFillColor(colors.black)
+    pdf.drawString(455, height - 515, "TOTAL :")
+    pdf.drawString(515, height - 515, "400€")
 
     # Ajouter les totaux et les conditions
     pdf.setFont("Helvetica-Bold", 12)
     pdf.drawString(65, height - 635, "Modalité de payement:")
+    pdf.setFillColor(colors.black)
     pdf.setFont("Helvetica", 9)
+    pdf.setFillColor(colors.darkslategrey)
     pdf.drawString(65, height - 650, "Un acompte de 100 euros est à verser pour confirmer la réservation avant le 30/10/2022.")
     pdf.drawString(65, height - 665, "Le reste est à payer au moment de la livraison ou au moins 2 jours avant l'évènement")
     pdf.drawString(65, height - 680, "Vous pouvez payer par virement (RIB à droite), Paylib, Paypal (paypal.me/3dmouvstudio)")
 
     pdf.setFont("Helvetica-Bold", 12)
+    pdf.setFillColor(colors.black)
     pdf.drawString(65, height - 710, "RIB:")
     pdf.setFont("Helvetica", 8)
+    pdf.setFillColor(colors.darkslategrey)
     pdf.drawString(65, height - 725, "TITULAIRE DU COMPTE : M STEPHANE FAURE")
     pdf.drawString(65, height - 740, "IBAN : FR58 3000 2069 5100 0000 6909 N52")
     pdf.drawString(65, height - 755, "BIC : CRLYFRPP")
