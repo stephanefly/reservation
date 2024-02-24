5283f07df19d8985f357f94cd8b1c445af2e11c8
stephanefly <faure_stephane@hotmail.fr>
2024-02-24 02:13:10 +0100

    graph

--- app/module/import_data/import_avoir.py
+++ app/module/import_data/import_avoir.py
@@ -8,10 +8,10 @@ from app.models import NameCost, Cost
 def upload_avoir():
     # Ouvrir le fichier JSON et charger les données
     with open(r'app\module\import_data\avoirs.json', 'r', encoding='utf-8') as file:
-        data_trello = json.load(file)
+        data_avoir = json.load(file)
 
     # Convertir les données JSON en DataFrame Pandas
-    df = pd.DataFrame(data_trello)
+    df = pd.DataFrame(data_avoir)
 
 
     names_uniques = df['Client'].unique()
