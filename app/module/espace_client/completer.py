06adb63f0ecf5797330fdc44bb2e16bf6e1c5a64
stephanefly <faure_stephane@hotmail.fr>
2024-07-18 02:20:04 +0200

    voguebooth

--- app/module/espace_client/completer.py
+++ app/module/espace_client/completer.py
@@ -6,26 +6,20 @@ from app.models import Event, EventDetails, EventTemplate
 
 
 @require_http_methods(["POST"])
-def update_event_and_redirect(request, event_id, data_type, update_field, redirect_view):
+def update_event_and_redirect(request, event_id, data_type_template, update_field, redirect_view):
     event = get_object_or_404(Event, pk=event_id)
-    new_data = request.POST.get(data_type)
+    new_data = request.POST.get(data_type_template)
 
-    # Vérifier si le champ à mettre à jour est event_details ou event_template
     if update_field == 'event_details':
-        model_instance = event.event_details
+        model_class = EventDetails
     elif update_field == 'event_template':
-        model_instance = event.event_template
+        model_class = EventTemplate
 
-
-    # Si l'instance du modèle existe, mettre à jour les données
-    if model_instance:
-        setattr(model_instance, data_type, new_data)
-        model_instance.save()
-    else:
-        # Si l'instance n'existe pas, en créer une nouvelle avec les données
-        model_class = EventDetails if update_field == 'event_details' else EventTemplate
-        setattr(event, update_field, model_class(**{data_type: new_data}))
-        event.save()
+    # Récupérer ou créer l'objet lié
+    model_instance, created = model_class.objects.get_or_create(event=event, defaults={data_type_template: new_data})
+    if not created:
+        setattr(model_instance, data_type_template, new_data)
+    model_instance.save()
 
     # Redirection vers l'URL "choix client" avec le token approprié
     client_token = request.session.get('client_token')
