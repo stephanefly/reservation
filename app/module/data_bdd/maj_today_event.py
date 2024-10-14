d13d3046805661b20feded2feafe103de2665725
stephanefly <faure_stephane@hotmail.fr>
2024-10-14 15:45:52 +0200

    tarifs maj

--- app/module/data_bdd/maj_today_event.py
+++ app/module/data_bdd/maj_today_event.py
@@ -1,38 +1,23 @@
 
 from app.models import Event, EventPostPrestation
 from django.utils import timezone
-from datetime import datetime
-
-
-# def maj_today_event():
-#     events_before_today = Event.objects.filter(
-#         event_details__date_evenement__lt=datetime.now(timezone.utc).date())
-#
-#     for event in events_before_today:
-#         if event.signer_at:
-#             event.status = "Post Presta"
-#             post_presta = EventPostPrestation()
-#             post_presta.save()  # On sauvegarde d'abord le post_presta avant de l'associer à l'event
-#             event.post_presta = post_presta
-#         else:
-#             event.status = "Refused"
-#         event.save()  # Ensuite, on sauvegarde l'event après avoir associé post_presta
-
+from datetime import datetime, timedelta
 
 
 def maj_today_event():
-    # Définir la date du 20 septembre 2024
-    start_date = datetime(2024, 9, 20).date()
-    # Obtenir la date actuelle
+
+    # Obtenir la date d'aujourd'hui
     today = datetime.now(timezone.utc).date()
 
-    # Filtrer les événements dont la date est entre le 20 septembre 2024 et aujourd'hui, avec le statut "Presta FINI" ou "Acompte OK"
-    events_in_range = Event.objects.filter(
-        event_details__date_evenement__range=(start_date, today),
-        status__in=["Presta FINI", "Acompte OK"]
+    # Calculer la date d'hier
+    yesterday = today - timedelta(days=1)
+
+    # Filtrer les événements dont la date est exactement celle d'hier
+    events_before_today = Event.objects.filter(
+        event_details__date_evenement=yesterday
     )
 
-    for event in events_in_range:
+    for event in events_before_today:
         if event.signer_at:
             event.status = "Post Presta"
             post_presta = EventPostPrestation()
@@ -41,3 +26,5 @@ def maj_today_event():
         else:
             event.status = "Refused"
         event.save()  # Ensuite, on sauvegarde l'event après avoir associé post_presta
+
+
