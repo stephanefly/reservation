54e5baa2810625b7ea13abf09c74590fc1868263
stephanefly <faure_stephane@hotmail.fr>
2024-10-30 13:25:54 +0100

    relance devis

--- app/module/data_bdd/update_event.py
+++ app/module/data_bdd/update_event.py
@@ -59,9 +59,7 @@ def update_data(event, request):
     event_option = event.event_option
 
     # Mise à jour des informations du page_client
-    raison_sociale = request.POST.get('raison_sociale')
-    if raison_sociale == 'on':
-        client.raison_sociale = True
+    client.raison_sociale = request.POST.get('raison_sociale') == 'on'
 
     client.nom = request.POST.get('client_nom')
     client.mail = request.POST.get('client_mail')
