1e4644bcb3d1734d949d3b63c8403faf5ff35d57
stephanefly <faure_stephane@hotmail.fr>
2025-01-29 02:10:26 +0100

    refactor mail code

--- myselfiebooth/sheduled_tasks/quotidien.py
+++ myselfiebooth/sheduled_tasks/quotidien.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
+
 
 # Chemin absolu du répertoire parent de 'myselfiebooth'
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -7,7 +8,6 @@ parent_dir = os.path.dirname(os.path.dirname(current_dir))
 sys.path.append(parent_dir)
 
 # Maintenant, vous pouvez importer 'myselfiebooth'
-import myselfiebooth
 
 # Configurer Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myselfiebooth.settings')
@@ -17,13 +17,16 @@ django.setup()
 
 # Importer et exécuter les tâches planifiées
 from app.module.data_bdd.maj_today_event import maj_today_event
-from app.module.espace_client.choose_to_relance import (choose_to_relance_espace_client,
-                                                        choose_to_relance_devis_client, choose_to_last_chance_devis_client)
+from app.module.mail.choose_to_relance import (choose_to_rappel_devis_client, choose_to_prolonger_devis_client,
+                                               choose_to_phonebooth_offert_devis_client, choose_to_last_chance_devis_client,
+                                               choose_to_relance_espace_client)
 
-# maj_today_event()
-# choose_to_relance_devis_client()
+maj_today_event()
+# choose_to_rappel_devis_client()
+# choose_to_prolonger_devis_client()
+# choose_to_phonebooth_offert_devis_client()
 choose_to_last_chance_devis_client()
-# choose_to_relance_espace_client()
+choose_to_relance_espace_client()
 
 # Backup BDD
 # launch_relance devis (2 jours hors vendredi/samedi/dimanche, 1 semaine plus tard, debut du mois prochain)
