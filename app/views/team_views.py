2a98b8df3f499da0c99ad72c9255b44ca18a8008
stephanefly <faure_stephane@hotmail.fr>
2025-05-15 01:46:21 +0200

    Media Collecté

--- app/views/team_views.py
+++ app/views/team_views.py
@@ -67,7 +67,7 @@ def view_image(request, event_id):
 def team_post_presta(request):
     lst_post_event = Event.objects.filter(
         signer_at__isnull=False,
-        status__in=['Post Presta','Sent Media']
+        status__in=['Post Presta']
     ).order_by('event_details__date_evenement')
 
     event_lst_member = get_member_list(lst_post_event)
