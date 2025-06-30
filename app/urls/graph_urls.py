a5cb2548f5af8b91c09cfe840982b0e50413e8cc
stephanefly <faure_stephane@hotmail.fr>
2025-06-30 14:34:41 +0200

    stratégie

--- app/urls/graph_urls.py
+++ app/urls/graph_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from ..views import graph, graph_cost, graph_cost_pie, graph_potentiel
+from ..views import graph, graph_cost, graph_cost_pie, graph_potentiel, cycle_strategique_view
 
 urlpatterns = [
     path('', graph, name='graph'),
     path('cost/', graph_cost, name='graph_cost'),
     path('cost/pie/', graph_cost_pie, name='graph_cost_pie'),
     path('potentiel', graph_potentiel, name='graph_potentiel'),
+    path('cycle-strategique/', cycle_strategique_view, name='cycle_strategique'),
 ]
--- app/views/graph_views.py
+++ app/views/graph_views.py
@@ -5,6 +5,8 @@ from ..module.lib_graph.lib_pie_chart import table_graph_pie
 from ..module.lib_graph.mise_en_week import new_mise_en_week, mise_en_week_avoir
 from ..module.lib_graph.get_data import get_ok_data, get_cost_data, get_ok_data_before
 from datetime import datetime, timedelta, timezone
+from ..models import Event
+
 
 today_date = datetime.now().date()
 date_now = today_date.strftime('%Y-%m-%d')
@@ -37,3 +39,22 @@ def graph_potentiel(request):
 
     script, div = tracage_figure_bar_potentiel(df_brut_net_now, df_brut_net_before, date_now)
     return render(request, 'app/backend/graph/graph_potentiel.html', {'script': script, 'div': div})
+
+def cycle_strategique_view(request):
+    today_date = datetime.now()
+    end_week_date = today_date + timedelta(days=7)
+
+    len_preparation = Event.objects.filter(
+        signer_at__isnull=False,
+        event_details__date_evenement__range=[today_date, end_week_date]
+    ).count()
+
+    len_conversion = Event.objects.filter(status__in=["Sended", "Resended",'Last Chance', 'First Rappel', 'Last Rappel', 'Prolongation', 'Temoignage', 'Phonebooth Offert']).count()
+    len_postprestation = Event.objects.filter(status__in=["Post Presta", 'Sent Media']).count()
+
+    return render(request, 'app/backend/graph/cycle_strategique.html',
+              {
+                  'len_preparation': len_preparation,
+                  'len_conversion': len_conversion,
+                  'len_postprestation': len_postprestation,
+              })
