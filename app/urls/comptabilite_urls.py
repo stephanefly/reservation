727397f8622cf949750f5dbc5b76aa5cf1a4ba19
stephanefly <faure_stephane@hotmail.fr>
2025-10-27 11:32:22 +0100

    remove comptabilite.html

--- app/urls/comptabilite_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from ..views import (
-    comptabilite
-)
-
-urlpatterns = [
-    path('lst_compta/', comptabilite, name='comptabilite'),
-]
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -7,5 +7,4 @@ from .graph_views import *
 from .tableau_de_bord_views import *
 from .post_presta_views import *
 from .team_views import *
-from .comptabilite_views import *
 
