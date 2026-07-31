ee716001cac369b094659ef8ade5911e011454ca
stephanefly <faure_stephane@hotmail.fr>
2026-08-01 01:19:04 +0200

    Modernise toutes les interfaces equipe

--- app/views/team_views.py
+++ app/views/team_views.py
@@ -1,30 +1,40 @@
 from django.views.decorators.http import require_http_methods
 from ..models import EventTemplate, Event, EventRelance, TeamMember
-from datetime import datetime, timedelta, timezone
-from django.shortcuts import redirect, get_object_or_404
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
-import os
+from datetime import timedelta
+from django.shortcuts import redirect, get_object_or_404, render
+from django.http import HttpResponseRedirect
 import json
-from ..module.cloud.create_timelaps import upload_file_to_pcloud
 from ..module.cloud.get_pcloud_data import get_pcloud_event_folder_data, upload_template_to_pcloud, \
     get_public_image_link_from_path
 from ..module.mail.send_mail_event import send_mail_event
-from django.template.loader import render_to_string
+from django.db.models import Prefetch
 
 from ..module.team.team_calendar import get_event_statuses, build_event_data, get_stock_machines, get_stock_options, \
     group_events_by_date
 
-today_date = datetime.now().date()
 from django.utils.timezone import now
 
 
+TEAM_EVENT_RELATIONS = (
+    'client',
+    'event_details',
+    'event_product',
+    'event_option',
+    'event_acompte',
+    'event_template',
+    'event_post_presta',
+)
+
+
+def _team_event_queryset():
+    return Event.objects.select_related(*TEAM_EVENT_RELATIONS)
+
+
 def template_to_do(request):
-    today_date = datetime.now()
+    today_date = now().date()
     end_week_date = today_date + timedelta(days=30)
 
-    lst_event_prio = Event.objects.filter(
+    lst_event_prio = _team_event_queryset().filter(
         signer_at__isnull=False,
         event_details__date_evenement__range=[today_date, end_week_date]
     ).order_by('event_details__date_evenement')
@@ -69,10 +79,10 @@ def view_image(request, event_id):
 
 
 def team_post_presta(request):
-    lst_post_event = Event.objects.filter(
+    lst_post_event = _team_event_queryset().filter(
         signer_at__isnull=False,
         status__in=['Post Presta']
-    ).order_by('event_details__date_evenement')
+    ).prefetch_related('event_team_members').order_by('event_details__date_evenement')
 
     return render(request, 'app/team/team_post_presta.html',
                   {
@@ -81,14 +91,14 @@ def team_post_presta(request):
 
 
 def team_planning(request):
+    today_date = now().date()
     today_minus_6h = today_date - timedelta(hours=6)
-
     end_week_date = today_date + timedelta(days=30)
 
-    lst_event_prio = Event.objects.filter(
+    lst_event_prio = _team_event_queryset().filter(
         signer_at__isnull=False,
         event_details__date_evenement__range=[today_minus_6h, end_week_date]
-    ).order_by('event_details__date_evenement')
+    ).prefetch_related('event_team_members').order_by('event_details__date_evenement')
 
     team_members = TeamMember.objects.all()
 
@@ -147,17 +157,23 @@ def calendar(request):
 
 
 def relance_client(request):
-    event_to_relance = Event.objects.filter(
+    event_to_relance = _team_event_queryset().filter(
         status__in=["Prolongation", "Last Chance", "Last Rappel", "Initied"]
-    ).select_related("event_details").order_by("event_details__date_evenement")
+    ).prefetch_related(
+        Prefetch(
+            'relances',
+            queryset=EventRelance.objects.order_by('-date_relance'),
+            to_attr='ordered_relances',
+        )
+    ).order_by("event_details__date_evenement")
 
     return render(request, 'app/team/relance_appel_client.html', {'event_to_relance': event_to_relance})
 
 
 def info_relance_client(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
+    event = get_object_or_404(_team_event_queryset(), pk=event_id)
     lst_relance_event = EventRelance.objects.filter(event=event).order_by('date_relance')
-    event_reduc_total = event.reduc_all + event.reduc_product
+    event_reduc_total = (event.reduc_all or 0) + (event.reduc_product or 0)
 
     if request.method == "POST":
         membre = request.POST.get("membre")
